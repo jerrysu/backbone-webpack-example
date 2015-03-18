f562d744b2774471234360e909df742647d39a4f
Jerry Su <jerry@remind101.com>
2015-03-18 11:04:59 -0700

    Install a bunch of loaders

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,23 @@ module.exports = {
   },
 
   module: {
+    preLoaders: [
+      {test: /\.jsx$/, loader: 'eslint', exclude: /node_modules/},
+    ],
     loaders: [
-      {test: /\.css$/, loaders: ['style', 'css']}
+      {test: /\.jsx?$/, loaders: ['react-hot', 'babel?experimental&blacklist[]=validation.react'], exclude: /node_modules/},
+      {test: /\.scss$/, loaders: ['style', 'css', 'autoprefixer', 'sass']},
+      {test: /\.css$/, loaders: ['style', 'css', 'autoprefixer']},
+      {test: /\.json$/, loaders: ['json']},
+      {test: /\.png$/, loader: 'url?limit=8192&mimetype=image/png'},
+      {test: /\.jpe?g$/, loader: 'url?limit=8192&mimetype=image/jpg'},
+      {test: /\.gif$/, loader: 'url?limit=8192&mimetype=image/gif'},
+      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=8192&mimetype=application/font-woff2'},
+      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=8192&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=8192&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=8192&mimetype=image/svg+xml'},
+      {test: /\.md$/, loader: 'remarkable'}
     ]
   }
 };
